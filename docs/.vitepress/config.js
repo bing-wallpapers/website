1b35be6448087fd7a8fe4d710e7123fd17e6fe8d
孟世博 <897205285@qq.com>
2023-10-05 12:34:58 +0800

    Update config.js

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from "vitepress";
-
-export default defineConfig({
+export default {
   title: "Bing Wallpapers",
   description: "Automatic daily collection Bing Wallpapers",
   head: [["link", { rel: "icon", type: "image/svg+xml", href: "/logo.png" }]],
-});
+  themeConfig: {
+    nav: [
+      { text: 'Github', link: 'https://github.com/bing-wallpapers/website' },
+    ],
+    footer: {
+      message: 'Released under the <a href="https://github.com/vuejs/vitepress/blob/main/LICENSE">MIT License</a>.',
+      copyright: 'Copyright © 2022-present <a href="https://github.com/exposir">Expo</a>'
+    },
+    search: {
+      provider: 'local'
+    }
+  }
+
+}
